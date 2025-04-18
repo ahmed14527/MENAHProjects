c97bbf983fe71b03dfd49ba8130c150d7248d0e1
ahmed14527 <108636071+ahmed14527@users.noreply.github.com>
2025-04-18 18:43:16 +0200

    add database

--- MENAHProjects/urls.py
+++ MENAHProjects/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path,include,re_path
 from drf_spectacular.views import (
@@ -40,3 +41,5 @@ urlpatterns = [
     
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
